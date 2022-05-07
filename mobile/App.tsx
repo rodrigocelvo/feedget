d8e654ed6a465acb92134116cc6d24ffdefb8b72
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2022-05-06 21:19:03 -0300

    add components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./src/theme";
+
+import { useFonts } from "expo-font";
+
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
+import AppLoading from "expo-app-loading";
+import Widget from "./src/components/Widget";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
@@ -13,8 +33,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colors.background,
   },
 });
